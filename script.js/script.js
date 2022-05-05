be5e1ac69f9280c63c873645fed44812d783f383
Romulo Max <maxromulo16@gmail.com>
2022-05-04 23:06:40 -0300

    segundo commit

--- script.js/script.js
+++ script.js/script.js
@@ -14,7 +14,12 @@ function abriMenu(){
     document.body.classList.add("menu-expanded")
 }
 
-
 function fechaMenu(){
     document.body.classList.remove("menu-expanded")
 }
+
+ScrollReveal({
+    origin:'top',
+    distance: '30px',
+    duration: '700'
+}).reveal('#homer, #homer .mulher,  #homer .numeros, #servico, #servico card, #servico cards' )
